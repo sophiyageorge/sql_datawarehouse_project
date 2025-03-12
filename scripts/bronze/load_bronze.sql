731595cd9f777ce64123b4ac0c33099b39e9b0eb
Mary Sophiya <47908915+sophiyageorge@users.noreply.github.com>
2025-03-12 17:58:09 +0530

    Update load_bronze.sql

--- scripts/bronze/load_bronze.sql
+++ scripts/bronze/load_bronze.sql
@@ -16,6 +16,9 @@ Usage Example:
     EXEC bronze.load_bronze;
 ===============================================================================
 */
+
+
+
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME,@batch_start_time DATETIME,@batch_end_time DATETIME;
